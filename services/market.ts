0135144a9a5d8b99d21cdc35b7c3d5299612b926
Sportex <korengkoles@gmail.com>
2026-04-03 00:47:54 +0700

    Create market.ts

---
+++ services/market.ts
@@ -0,0 +1,7 @@
+export function buyStake(team: any, amount: number) {
+  console.log(`Bought ${amount} stake in ${team.name}`);
+}
+
+export function sellStake(team: any, amount: number) {
+  console.log(`Sold ${amount} stake in ${team.name}`);
+}
