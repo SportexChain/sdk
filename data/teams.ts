887c1d8e3141066ecc799f97af4d60459554e7df
Sportex <korengkoles@gmail.com>
2026-04-03 00:48:15 +0700

    Create teams.ts

---
+++ data/teams.ts
@@ -0,0 +1,20 @@
+export const teams = [
+  {
+    id: 1,
+    name: "Manchester United",
+    price: 120,
+    performance: "+5%"
+  },
+  {
+    id: 2,
+    name: "Real Madrid",
+    price: 150,
+    performance: "+8%"
+  },
+  {
+    id: 3,
+    name: "Barcelona",
+    price: 130,
+    performance: "-2%"
+  }
+];
